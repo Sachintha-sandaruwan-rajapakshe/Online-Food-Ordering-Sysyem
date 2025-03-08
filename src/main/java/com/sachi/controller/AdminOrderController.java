9ef508b9697f7213ca87f9e108b37c62a45fd927
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2025-03-08 14:16:12 +0530

    completed all the API for Food Ordering System

---
+++ src/main/java/com/sachi/controller/AdminOrderController.java
@@ -0,0 +1,48 @@
+package com.sachi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sachi.Model.Order;
+import com.sachi.Model.User;
+import com.sachi.service.OrderService;
+import com.sachi.service.UserService;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminOrderController {
+	@Autowired
+	private OrderService orderService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/order/restaurent/{id}")
+	public ResponseEntity<List<Order>> fetOrderHistory(@PathVariable Long id,@RequestHeader("Authorization")String jwt,@RequestParam(required = false) String orderStatus ) throws Exception{
+		
+		User user =userService.findUserByJwtToken(jwt);
+		List<Order> order =orderService.getRestaurentsOrder(id, orderStatus);
+		return new ResponseEntity<List<Order>>(order,HttpStatus.OK);
+		
+	}
+	
+	@PutMapping("/order/{orderId}/{orderStatus}")
+	public ResponseEntity<Order> updateOrderStatus(@PathVariable Long id,@PathVariable String orderStatus, @RequestHeader("Authorization")String jwt) throws Exception{
+		
+		User user =userService.findUserByJwtToken(jwt);
+		Order order =orderService.updateOrder(id, orderStatus);
+		return new ResponseEntity<Order>(order,HttpStatus.OK);
+		
+	}
+
+}
---
+++ src/main/java/com/sachi/controller/OrderController.java
@@ -0,0 +1,52 @@
+package com.sachi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sachi.Model.CartItem;
+import com.sachi.Model.Order;
+import com.sachi.Model.User;
+import com.sachi.request.AddCartItemRequest;
+import com.sachi.request.OrderRequest;
+import com.sachi.service.OrderService;
+import com.sachi.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+	@Autowired
+	private OrderService orderService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/order")
+	public ResponseEntity<Order> acreateOrder(@RequestBody OrderRequest req,@RequestHeader("Authorization")String jwt ) throws Exception{
+		
+		User user =userService.findUserByJwtToken(jwt);
+		Order order =orderService.createOrder(req, user);
+		return new ResponseEntity<Order>(order,HttpStatus.CREATED);
+		
+	}
+	
+	@GetMapping("/order/user")
+	public ResponseEntity<List<Order>> fetOrderHistory(@RequestBody OrderRequest req,@RequestHeader("Authorization")String jwt ) throws Exception{
+		
+		User user =userService.findUserByJwtToken(jwt);
+		List<Order> order =orderService.getUsersOrder(user.getId());
+		return new ResponseEntity<List<Order>>(order,HttpStatus.OK);
+		
+	}
+	
+
+}
