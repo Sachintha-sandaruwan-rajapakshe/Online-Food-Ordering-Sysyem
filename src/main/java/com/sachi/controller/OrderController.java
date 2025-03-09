c22584adb6d58d031eb4ac952356919c2ccf8ffb
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2025-03-09 21:39:33 +0530

    changers

--- src/main/java/com/sachi/controller/OrderController.java
+++ src/main/java/com/sachi/controller/OrderController.java
@@ -7,16 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sachi.Model.CartItem;
 import com.sachi.Model.Order;
 import com.sachi.Model.User;
-import com.sachi.request.AddCartItemRequest;
 import com.sachi.request.OrderRequest;
 import com.sachi.service.OrderService;
 import com.sachi.service.UserService;
