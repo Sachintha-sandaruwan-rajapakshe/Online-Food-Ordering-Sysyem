13e92159447708185e71f610aef1b67942b9e46c
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2025-03-18 21:19:24 +0530

    card section

--- frontend/src/component/Restaurent/MenuCard.jsx
+++ frontend/src/component/Restaurent/MenuCard.jsx
@@ -11,7 +11,18 @@ const MenuCard = () => {
     aria-controls="panel1-content"
     id="panel1-header"
   >
-    Accordion
+    <div className='lg:flex items-center justify-bitween'>
+      <div className='lg:flex items-center lg:gap-5'>
+        <img className='w-[7rem] h-[7rem] object-cover'
+          src="https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_640.jpg" alt="" />
+      </div>
+      <div className='space-y-1 lg:space-y-5 lg:max-w-2xl'>
+        <p className='font-semibold text-xl'>Fish Deval</p>
+        <p>Rs.590.00</p>
+        <p className='text-gray-400'>Nice food </p>
+      </div>
+
+    </div>
   </AccordionSummary>
   <AccordionDetails>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
