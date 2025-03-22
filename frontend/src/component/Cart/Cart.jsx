37072234762470f2d77f132bfa7d474b843b675a
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2025-03-22 21:38:16 +0530

    modify cart section

--- frontend/src/component/Cart/Cart.jsx
+++ frontend/src/component/Cart/Cart.jsx
@@ -1,14 +1,21 @@
 import { Divider } from '@mui/material'
 import React from 'react'
+import CartItem from './CartItem'
 
+const items=[1,1];
 const Cart = () => {
   return (
     <main className='lg:flex justify-between'> 
         <section className='lg:w-[30%] space-y-6 lg:min-h-screen pt-10'>
-
+          <CartItem/>
+          {items.map((item)=><CartItem/>)}
+          <Divider/>
+          <div className='billDetails px-5 text-sm'>
+            <p className='font-extralight py-5'>Bill Details</p>
+          </div>
         </section>
-        <Divider/>
-
+        
+        
     </main>
   )
 }
--- frontend/src/component/Cart/CartItem.jsx
+++ frontend/src/component/Cart/CartItem.jsx
@@ -1,4 +1,6 @@
-import { IconButton } from '@mui/material'
+import {  Chip, IconButton } from '@mui/material'
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import React from 'react'
 
 const CartItem = () => {
@@ -16,16 +18,26 @@ const CartItem = () => {
                     <div className='flex justify-between items-center'>
                         <div className='flex items-center space-x-1'> 
                             <IconButton>
-                                
+                                <RemoveCircleOutlineOutlinedIcon/>
+                            </IconButton>
+
+                            <div className='w-5 h-5 text-xs flex items-center justify-between'>
+                                {5}
+                            </div>
+
+                            <IconButton>
+                                <AddCircleOutlineIcon/>
                             </IconButton>
                         </div>
+                        
                     </div>
                 </div>
-
+                <p>Rs.200.00</p>
             </div>
-
         </div>
-      
+        <div className='pt-3 space-x-2'>
+                {[1,1,1].map((item)=><Chip label={'bread'}/>)}
+        </div>
     </div>
   )
 }
