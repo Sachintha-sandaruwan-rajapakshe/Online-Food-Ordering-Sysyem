ba350b3944dca33521f4f06565f5cddbe87491b0
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2025-05-20 22:10:51 +0530

    login and register

--- frontend/src/component/Auth/LoginForm.jsx
+++ frontend/src/component/Auth/LoginForm.jsx
@@ -1,9 +1,62 @@
+import { Button, TextField, Typography } from '@mui/material'
+import { Field, Form, Formik } from 'formik'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const initialValues = {
+  email: '',
+  password: ''
+}
+
+const handleSubmit = (values) => {
+  console.log(values)
+  // handle login logic here
+}
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      
+      <Typography variant='h5' className='text-center'>
+        Login
+      </Typography>
+
+      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+        {() => (
+          <>
+            <Form>
+              <Field
+                as={TextField}
+                name="email"
+                label="Email"
+                fullWidth
+                variant="outlined"
+                margin="normal"
+              />
+
+              <Field
+                as={TextField}
+                name="password"
+                label="Password"
+                type="password"
+                fullWidth
+                variant="outlined"
+                margin="normal"
+              />
+
+              <Button sx={{ mt: 2 }} variant='contained' fullWidth type='submit'>
+                Login
+              </Button>
+            </Form>
+
+            <Typography variant='body2' align='center' sx={{ mt: 3 }}>
+              Don't have an account?{' '}
+              <Button onClick={() => navigate("/account/:register")}>Register</Button>
+            </Typography>
+          </>
+        )}
+      </Formik>
     </div>
   )
 }
