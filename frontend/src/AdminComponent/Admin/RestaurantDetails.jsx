cdbb19019b87f1e6ae8b8772c2819717fd8d6aed
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2025-08-23 22:26:32 +0530

    create restaurant details page

--- frontend/src/AdminComponent/Admin/RestaurantDetails.jsx
+++ frontend/src/AdminComponent/Admin/RestaurantDetails.jsx
@@ -1,9 +1,159 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Restaurentdetails.css';
+import { Button, Card, CardContent, CardHeader, Grid, Icon, IconButton } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 const RestaurantDetails = () => {
+  const [restaurantStatus,setRestaurantStatus]=useState();
+  const handleRestaurantStatus=(value)=>{
+    setRestaurantStatus(value);
+  }
   return (
-    <div>
-      RestaurantDetails
+    <div className='lg:px-20 px-5'>
+      <div className='py-5 flex justify-center items-center gap-5'>
+        <h1 className='text-2xl lg:text-6xl text-center font-bold p-5 blackadder'> Sachi Food restaurant</h1>
+
+        <div className=''>
+          <IconButton>
+            <Button color={true?'primary':'error'} onClick={handleRestaurantStatus} size='medium'variant='outlined'>
+              {true?"close":"Open"}
+            </Button>
+          </IconButton>
+        </div>
+      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}> 
+          <Card>
+            <CardHeader title={<span className='text-gray-300'>Restaurant</span>}/>
+            <CardContent>
+              <div className='space-y-4 text-gray-200'>
+
+                <div className='flex'>
+                  <p className='w-48'>Owner</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>sachintha sandaruwan
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-48'>Restaurant Name</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>sachi food restaurant
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-48'>Cuisune Type</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>Srilankan
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-48'>Opening hours</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>9.00 am - 9.00.pm
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-48'>Status</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>
+                    {true?<span className='px-5 py-2 rounded-full bg-green-400 text-gray-950'>open</span>:<span className='px-5 py-2 rounded-full bg-red-400 text-gray-950'>close</span>}
+                  </p>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={6}> 
+          <Card>
+            <CardHeader title={<span className='text-gray-300'>Address</span>}/>
+            <CardContent>
+              <div className='space-y-4 text-gray-200'>
+
+                <div className='flex'>
+                  <p className='w-48'>Country</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>Srilanka
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-48'>City</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>Bandarawela
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-48'>Postal Code</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>10300
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-48'>Street Address</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>30/2 ,Bandarwela road 
+                  </p>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={6}> 
+          <Card>
+            <CardHeader title={<span className='text-gray-300'>Contact</span>}/>
+            <CardContent>
+              <div className='space-y-4 text-gray-200'>
+
+                <div className='flex'>
+                  <p className='w-20'>Email</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>sachinthasandaruwan8675@gmail.com
+                  </p>
+                </div>
+
+                <div className='flex'>
+                  <p className='w-20'>Mobile</p>
+                  <p className='text-gray-400'>
+                    <span className='pr-5'>-</span>+94 71 244 6924
+                  </p>
+                </div>
+
+                <div className='flex' row>
+                  <p className='w-20'>Social</p>
+                  <div className='items-center pb-4'>
+                    <span className='pr-5'>-</span>
+                    <a className='pr-5'href="http://">
+                        <InstagramIcon sx={{fontSize:"3rem"}}/>
+                      </a>
+
+                      <a className='pr-5'href="http://">
+                        <TwitterIcon sx={{fontSize:"3rem"}}/>
+                      </a>
+
+                      <a className='pr-5'href="http://">
+                        <LinkedInIcon sx={{fontSize:"3rem"}}/>
+                      </a>
+                      <a href="http://">
+                        <FacebookIcon sx={{fontSize:"3rem"}}/>
+                      </a>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   )
 }
--- frontend/src/AdminComponent/Events/Events.jsx
+++ frontend/src/AdminComponent/Events/Events.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import EventCard from './EventCard'
+import { Create } from '@mui/icons-material'
+import { Card, IconButton, Typography } from '@mui/material'
 
 const Events = () => {
   return (
-    <div className='mt-5 px-5 flex flex-wrap gap-5'>
-      {[0,1,2].map((item)=><EventCard/>)}
+    <div>
+      <Card sx={{ px: 5 }}>
+        <div className="flex items-center justify-between py-4">
+          <Typography variant="h5">Events</Typography>
+          <IconButton>
+            <Create />
+          </IconButton>
+        </div>
+      </Card>
+
+      <div className='mt-5 px-5 flex flex-wrap gap-5'>
+        {[0,1,2].map((item)=><EventCard/>)}
+      </div>
     </div>
+    
   )
 }
 
